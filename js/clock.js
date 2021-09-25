27173d55b94ebdd4afb07b7a806e5922bf405090
Kim-Hyeong-joon <gudwns1020@naver.com>
2021-09-25 18:51:36 +0900

    Update Clock

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,12 @@
-const clcok = document.querySelector("#clock");
+const clock = document.querySelector("#clock");
 
 function getClock() {
   const date = new Date();
-  clcok.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
