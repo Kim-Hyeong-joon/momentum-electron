f61d004bfd3e80896739c0df34a5faf5110d03d5
Kim-Hyeong-joon <gudwns1020@naver.com>
2021-10-14 00:24:50 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,21 @@
+API_KEY = "576bc34d6fe29d30ac7e99865f1d352a";
+
 function onGeoOk(position) {
-  console.log(position);
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
+      city.innerText = data.name;
+    });
 }
 
 function onGeoError() {
   alert("can't find you. No weather for you.");
 }
 
-console.log(navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError));
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
