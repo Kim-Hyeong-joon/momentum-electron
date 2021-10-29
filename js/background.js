ae5588ddfc2a6ce8c370cde5cbe9ea700113914b
Kim-Hyeong-joon <gudwns1020@naver.com>
2021-10-29 15:43:42 +0900

    Update bg-image

--- js/background.js
+++ js/background.js
@@ -1,4 +1,13 @@
-const images = ["0.JPG", "1.JPG", "2.JPG"];
+const images = [
+  "0.JPG",
+  "1.JPG",
+  "2.JPG",
+  "3.JPG",
+  "4.JPG",
+  "5.JPG",
+  "6.JPG",
+  "7.JPG",
+];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
