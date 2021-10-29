e2a9c67c73c288cbdd7befbe770c6b72c50d25c9
Kim-Hyeong-joon <gudwns1020@naver.com>
2021-10-29 15:58:57 +0900

    Update CSS

--- js/background.js
+++ js/background.js
@@ -1,13 +1,4 @@
-const images = [
-  "0.JPG",
-  "1.JPG",
-  "2.JPG",
-  "3.JPG",
-  "4.JPG",
-  "5.JPG",
-  "6.JPG",
-  "7.JPG",
-];
+const images = ["0.JPG", "1.JPG", "2.JPG", "3.JPG", "4.JPG", "6.JPG", "7.JPG"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
